337dadd6d925ecbe579f56eb810c7a9fe496adee
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-06-02 14:24:54 +0200

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
+import Papa from "papaparse"
 
 function App() {
+  const [tableRows, setTableRows] = useState([])
+  const [values, setValues] = useState([])
+
+  const changeHandler = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        const rowsArray = []
+        const valuesArray = []
+
+        results.data.map((d) => {
+          rowsArray.push(Object.keys(d))
+          return valuesArray.push(Object.values(d))
+        })
+
+        setTableRows(rowsArray[0])
+        setValues(valuesArray)
+      },
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={changeHandler}
+        style={{ display: "block", margin: "10px auto" }}
+      />
+      {/* <table>
+        <thead>
+          <tr>
+            {tableRows.map((rows, index) => {
+              return <th key={index}>{rows}</th>
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {values.map((value, index) => (
+            <tr key={index}>
+              {value.map((val, i) => (
+                <td key={i}>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
