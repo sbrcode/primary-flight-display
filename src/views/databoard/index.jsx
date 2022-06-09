5b7bcc5a9c211e4cfb3ecf0740b75647471be55e
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-06-09 17:22:34 +0200

    add-prop-datagrid

--- src/views/databoard/index.jsx
+++ src/views/databoard/index.jsx
@@ -1,20 +1,19 @@
 import { DataGrid, GridToolbar } from "@mui/x-data-grid"
 import { Headers, Rows } from "../../data"
 
-const DataBoard = () => {
-  return (
-    <div className="App" style={{ height: 900 }}>
-      <DataGrid
-        rows={Rows}
-        columns={Headers}
-        pageSize={15}
-        rowsPerPageOptions={[15]}
-        components={{
-          Toolbar: GridToolbar,
-        }}
-      />
-    </div>
-  )
-}
+const DataBoard = () => (
+  <div className="App" style={{ height: 900 }}>
+    <DataGrid
+      rows={Rows}
+      columns={Headers}
+      pageSize={15}
+      rowsPerPageOptions={[15]}
+      checkboxSelection
+      components={{
+        Toolbar: GridToolbar,
+      }}
+    />
+  </div>
+)
 
 export default DataBoard
